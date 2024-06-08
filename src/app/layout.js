4a2d77775c8df946c697ca25c5219365f466d1d5
BhargavaRam123 <allabhargavaram200@gmail.com>
2024-06-09 03:59:52 +0530

    half of message getting functionality achieved

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,8 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <GoogleOAuthProvider clientId="801064546104-59ceuepf5flhunktpf3f5876og9jlqrg.apps.googleusercontent.com">
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </GoogleOAuthProvider>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,54 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { GoogleLogin } from "@react-oauth/google";
+import { apiconnector } from "./services/apiconnector";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
 export default function Home() {
+  const [accesstoken, setaccesstoken] = useState("");
+  async function getemailinfo(){
+    const response = await apiconnector("GET","https://gmail.googleapis.com/gmail/v1/users/me/profile",
+  null,   
+  {
+    Authorization: `Bearer ${accesstoken}`,
+  }   
+    )
+    console.log('response:',response)
+  }
+  
+  async function getmessages(){
+    const response = await apiconnector("GET","https://gmail.googleapis.com/gmail/v1/users/me/messages",
+  null,   
+  {
+    Authorization: `Bearer ${accesstoken}`,
+  }   
+    )
+    console.log('response:',response)
+  }
+
+  const login = useGoogleLogin({
+    scope:
+      "https://mail.google.com",
+    onSuccess: (tokenResponse) => {
+      setaccesstoken(tokenResponse.access_token);
+      console.log(tokenResponse);
+    },
+  });
   return (
     <div>
-      hello
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+      {/* for the below i am getting the access token */}
+      <button onClick={() => login()}>Sign in with Google 🚀</button>
+      <button onClick={getemailinfo}>test make a call</button>
+      <button onClick={getmessages}>test to get the messages</button>
     </div>
   );
 }
